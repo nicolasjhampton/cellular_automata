f705afa7a55c56e0dd47c137b1ddc55071a5328a
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-12 23:40:08 -0700

    switched default rule from 110 to 126

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const automatonWorker = new Worker('js/automatonworker.js');
 /**
  * CONSTANTS
  */
-const RULE110 = new Uint16Array([0, 1, 1, 0, 1, 1, 1, 0]);
+const RULE126 = new Uint16Array([0, 1, 1, 1, 1, 1, 1, 0]);
 const WIDTH = Uint16Array.of(1000);
 const HEIGHT = Uint16Array.of(600);
 const COLUMNS = Uint16Array.of(1000);
@@ -17,7 +17,7 @@ const FIRST_YEAR = generateRandomYear();
 /**
  * CONSTANTS copies
  */
-const RULE110_COPY = RULE110.slice();
+const RULE126_COPY = RULE126.slice();
 const WIDTH_COPY = WIDTH.slice();
 const HEIGHT_COPY = HEIGHT.slice();
 const FIRST_YEAR_COPY = FIRST_YEAR.slice();
@@ -46,8 +46,8 @@ const context = canvasOnScreen.getContext('2d', { alpha: true });
  */
 canvasWorker.postMessage({ eventName: 'init', WIDTH, HEIGHT, COLUMNS, FIRST_YEAR}, 
                          [ WIDTH.buffer, HEIGHT.buffer, COLUMNS.buffer, FIRST_YEAR.buffer ]);
-automatonWorker.postMessage({ eventName: 'init', FIRST_YEAR_COPY, RULE110}, 
-                            [ FIRST_YEAR_COPY.buffer, RULE110.buffer ]);
+automatonWorker.postMessage({ eventName: 'init', FIRST_YEAR_COPY, RULE126}, 
+                            [ FIRST_YEAR_COPY.buffer, RULE126.buffer ]);
 
 
 /**
@@ -92,7 +92,7 @@ const domHandlers = DOMHandlerFactory({
     buttonClass: 'pixel-switch',
     onClass: 'on'
 });
-domHandlers.initRuleBtns(RULE110_COPY, (newRule) => {
+domHandlers.initRuleBtns(RULE126_COPY, (newRule) => {
     automatonWorker.postMessage({ eventName: 'replaceRule', newRule }, [newRule.buffer]);
 });
 domHandlers.seedYearBtn((e) => {
